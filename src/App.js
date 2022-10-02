e876468091844e16bffd66334f8efdecadf765c9
clxrityy <97744702+clxrityy@users.noreply.github.com>
2022-10-02 00:28:45 -0400

    change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src='https://avatars.githubusercontent.com/u/97744702?v=4' className="App-logo" alt="logo" />
+        <h1>cl<span>x</span>rity</h1>
+        <p className='body1'>
+          do what makes you happy 🍀
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/clxrityy/react-test"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub
         </a>
       </header>
     </div>
