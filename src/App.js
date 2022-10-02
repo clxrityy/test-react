b4a6a644be536b4cf5e75683e66df3eefe12bb85
clxrityy <clxrityxyz@gmail.com>
2022-10-02 15:46:30 -0400

    git

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
->>>>>>> e876468091844e16bffd66334f8efdecadf765c9
 import './App.css';
 
 function App() {
@@ -8,14 +5,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src='https://avatars.githubusercontent.com/u/97744702?v=4' className="App-logo" alt="logo" />
-<<<<<<< HEAD
+
         <p>
           just for fun
         </p>
-        <a
-          className="App-link"
-          href="https://github.com/clxrityy/clxrity.xyz"
-=======
         <h1>cl<span>x</span>rity</h1>
         <p className='body1'>
           do what makes you happy 🍀
@@ -23,7 +16,6 @@ function App() {
         <a
           className="App-link"
           href="https://github.com/clxrityy/react-test"
->>>>>>> e876468091844e16bffd66334f8efdecadf765c9
           target="_blank"
           rel="noopener noreferrer"
         >
